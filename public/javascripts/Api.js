fc5a1766b6f378a24075be8dabb15b0eb7bd23ee
AnttiKayhko <antti.kayhko@metropolia.fi>
2019-03-17 16:47:37 +0200

    Initial commit

---
+++ public/javascripts/Api.js
@@ -0,0 +1,12 @@
+function getBookData(bookid){// nimi kuva kieli author chapterit description
+
+}
+function getBookRating(bookid){
+
+}
+function getBookGenres(bookid){
+
+}
+function getBookTags(bookid){
+
+}
---
+++ routes/index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+var api = require('./routes/index')
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  var bname, bpic, bgenres, btags ,brating,blanguage, bauthor,bchapters,bdescription
+  res.render('index', { title: 'Books for everyone', bookname: bname, bookpic:bpic, bookgenres:bgenres, booktags:btags, bookrating:brating, booklanguage:blanguage, bookauthor:bauthor, bookchapters:bchapters,bookdescription:bdescription });
+});
+router.get('/book/:bookid', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+
+
+module.exports = router;
