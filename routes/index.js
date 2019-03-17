bdc6687fc1c81a1992be25d21d931e50038acf28
AnttiKayhko <antti.kayhko@metropolia.fi>
2019-03-18 01:26:21 +0200

    Sivun pystyy nyt generoimaan layoutin ja tietokannan perusteella
    eli pystytään laittlamaan localhost/8081/book/kijannimi ja ladata suoraan tiedot sivuun
    
    kirjoja pystyy nyt hakemaan tagellä ja genreillä

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,51 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  var bname, bpic, bgenres, btags ,brating,blanguage, bauthor,bchapters,bdescription
-  res.render('index', { title: 'Books for everyone', bookname: bname, bookpic:bpic, bookgenres:bgenres, booktags:btags, bookrating:brating, booklanguage:blanguage, bookauthor:bauthor, bookchapters:bchapters,bookdescription:bdescription });
+router.get('/book/:name', function(req, res, next) {
+  // var bname, bpic, bgenres, btags ,brating,blanguage, bauthor,bchapters,bdescription;
+  request("http://localhost:8081/v1/api/book/"+req.params.name, function (error, response, body) {
+    testlogger(error, response, body);
+
+    var data = JSON.parse(body);
+    var bookdata = data.response[0];
+    // console.log(bookdata);
+
+    request("http://localhost:8081/v1/api/book/"+req.params.name+"/genres", function (error, response, body) {
+      testlogger(error, response, body);
+
+      var genredata = JSON.parse(body);
+      var bookgenres = genredata.response;
+
+      request("http://localhost:8081/v1/api/book/"+req.params.name+"/tags", function (error, response, body) {
+        testlogger(error, response, body);
+
+        var tagdata = JSON.parse(body);
+        var booktags = tagdata.response;
+
+        request("http://localhost:8081/v1/api/book/"+req.params.name+"/rating", function (error, response, body) {
+          testlogger(error, response, body);
+
+          var ratingdata = JSON.parse(body);
+          console.log(bookdata);
+          console.log("before render");
+          res.render('index', { title: 'Books for everyone', data: bookdata, genres: bookgenres, tags: booktags, rating: ratingdata.response[0]});
+
+        });
+
+
+      });
+
+    });
+  });
 });
+function testlogger(error, response, body){
+  console.log('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  console.log('body:', body); // Print the HTML for the Google homepage.)
+}
 router.get('/book/:bookid', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
--- routes/restApi.js
+++ routes/restApi.js
@@ -50,7 +50,7 @@ router.get('/book/:name/genres', function(req, res, next) {
   });
 });
 router.get('/book/:name/tags', function(req, res, next) {
-  var sql ="select tags.tagName\n" +
+  var sql ="select tags.tagName, tags.tagDescription\n" +
       "from tags\n" +
       "\tjoin tags_books\n" +
       "\t\ton tags.id = tags_books.Tags_id\n" +
@@ -88,5 +88,53 @@ router.get('/book/:name/rating', function(req, res, next) {
     }
   });
 });
+
+
+router.get('/genre/:name', function(req, res, next) {
+  var sql ="select books.bookName, books.bookDescription, books.bookPicture_url, books.bookChapter_count\n" +
+      "from books\n" +
+      "\t\tjoin books_genres\n" +
+      "\t\t\ton books.id = books_genres.Books_id\n" +
+      "\t\tjoin genres\n" +
+      "\t\t\ton books_genres.Genres_id = genres.id\n" +
+      "where genres.genreName like ?";
+
+  res.locals.connection.query(sql,req.params.name+"%", function (error, results, fields) {
+    if (error) throw error;
+
+    if(error){
+      res.send(JSON.stringify({"status": 500, "error": error, "response": null}));
+      //If there is error, we send the error in the error section with 500 status
+    } else {
+      res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+      //If there is no error, all is good and response is 200OK.
+    }
+  });
+});
+
+router.get('/tag/:name', function(req, res, next) {
+  var sql ="select books.bookName, books.bookDescription, books.bookPicture_url, books.bookChapter_count\n" +
+      "from books\n" +
+      "\t\tjoin tags_books\n" +
+      "\t\t\ton books.id = tags_books.Books_id\n" +
+      "\t\tjoin tags\n" +
+      "\t\t\ton tags_books.Tags_id = tags.id\n" +
+      "where tags.tagName like ?";
+
+  res.locals.connection.query(sql,req.params.name+"%", function (error, results, fields) {
+    if (error) throw error;
+
+    if(error){
+      res.send(JSON.stringify({"status": 500, "error": error, "response": null}));
+      //If there is error, we send the error in the error section with 500 status
+    } else {
+      res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+      //If there is no error, all is good and response is 200OK.
+    }
+  });
+});
+
+
+
 
 module.exports = router;
